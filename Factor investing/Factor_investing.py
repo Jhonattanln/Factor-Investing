0f034e77b9b52908482fefef8f0b0cb2ccb262bf
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-11-25 20:40:23 -0300

    Update Factor_investing.py

--- Factor investing/Factor_investing.py
+++ Factor investing/Factor_investing.py
@@ -9,13 +9,10 @@ dy = pd.read_excel(r'DY.xlsx', parse_dates=True, index_col=0).dropna()
 dy.astype(float)
 
 def columns(df):
-    df.index = df.index.str.replace('[iv ld (inic)\n1 anos\nm moeda orig\n]', '')
+    df.index = df.index.str[-5:]
     return df
 columns(dy)
 
 dy.sort_values(by=['Dividend Yield'], ascending=False, inplace=True)
 assets = dy.iloc[0:10]
-assets
-
-assets.index = assets.index.str[-5:]
 assets
