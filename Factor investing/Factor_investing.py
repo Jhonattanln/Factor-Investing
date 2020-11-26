e784707e167a3e0e896f9dc79ee73d2a040039a2
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-11-25 22:00:52 -0300

    Update Factor_investing.py

--- Factor investing/Factor_investing.py
+++ Factor investing/Factor_investing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas_datareader import data
 
 ################################################### Dividend Yield ################################################
 
@@ -15,4 +16,17 @@ columns(dy)
 
 dy.sort_values(by=['Dividend Yield'], ascending=False, inplace=True)
 assets = dy.iloc[0:10]
-assets
+assets.index
+
+stocks = ['CGAS5.SA', 'ENAT3.SA', 'SEER3.SA', 'WIZS3.SA', 'ROMI3.SA', 'CYRE3.SA', 'DIRR3.SA', 'WHRL4.SA',
+       'CSNA3.SA', 'GEPA4.SA', '^BVSP']
+df = pd.DataFrame()
+for i in stocks:
+    df[i] = data.DataReader(i, data_source='yahoo', start='2020-01-01')['Adj Close']
+
+norm = pd.DataFrame()
+for i in df:
+    norm[i] = df[i].div(df[i].iloc[0]).mul(100)
+norm.plot()
+plt.legend(loc='lower left')
+plt.show()
