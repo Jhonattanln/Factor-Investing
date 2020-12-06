0db3853816f9a7127ae5aab88cbdb19a94b9dc07
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-12-05 21:58:13 -0300

    Update Size.py

--- Factor investing/Size/Size.py
+++ Factor investing/Size/Size.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas_datareader import data
 
+########################################################## Size ###################################################################
+
+size = pd.read_excel(r'Size.xlsx')
