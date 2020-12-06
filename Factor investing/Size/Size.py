156d150ec50de83e49e3fd48da426cdc4fa70384
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-12-05 23:26:10 -0300

    Update Size.py

--- Factor investing/Size/Size.py
+++ Factor investing/Size/Size.py
@@ -5,4 +5,14 @@ from pandas_datareader import data
 
 ########################################################## Size ###################################################################
 
-size = pd.read_excel(r'Size.xlsx')
+size = pd.read_excel(r'C:\Users\Jhona\OneDrive - Grupo Marista\Projetos\Factor Investing\Factor-Investing\Factor investing\Size\Size.xlsx', 
+                     parse_dates=True, index_col=0).dropna()
+def columns(df):
+    df.columns = df.columns.str[-6:]
+    return df
+
+columns(size)
+size = size.T
+
+size.rename(columns={2019-12-31: 'Market Cap'}, inplace=True)
+size
