de3782e3310f96866d0b8bb3537b3cface2b85c8
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-12-05 23:29:58 -0300

    Update Size.py

--- Factor investing/Size/Size.py
+++ Factor investing/Size/Size.py
@@ -13,6 +13,45 @@ def columns(df):
 
 columns(size)
 size = size.T
+size.replace('-', np.nan, inplace=True)
 
-size.rename(columns={2019-12-31: 'Market Cap'}, inplace=True)
-size
+size.columns = size.columns.astype(str)
+size.rename(columns={'2019-12-31': 'Market Cap'}, inplace=True)
+size.sort_values('Market Cap', inplace=True)
+
+assets = size.iloc[4:14]
+assets.index
+
+stocks = ['ATOM3.SA', 'IGBR3.SA', 'LUPA3.SA', 'PDGR3.SA', 'SLED4.SA', 'BTTL3.SA',
+       'VIVR3.SA', 'RCSL4.SA', 'ETER3.SA', 'RSID3.SA']
+
+df = pd.DataFrame()
+for i in stocks:
+    df[i] = data.DataReader(i, data_source='yahoo', start='2020-01-01')['Adj Close']
+
+weights = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
+returns = df.pct_change().dropna()
+
+df['Portfolio'] = (1+returns.dot(weights)).cumprod().dropna()
+
+norm = pd.DataFrame()
+for i in df:
+    norm[i] = df[i].div(df[i].iloc[1]).mul(100)
+norm
+
+plt.style.use('ggplot')
+norm.plot()
+plt.legend(loc='lower left')
+plt.show()
+
+### Portfolio vs IBOV
+ibov = data.DataReader('^BVSP', data_source='yahoo', start='2020-01-01')
+ibov.rename(columns = {'Adj Close':'IBOV'}, inplace=True)
+ibov.drop(ibov.columns[[0,1,2,3,4]], axis=1, inplace=True)
+ibov['Ibov'] = ibov['IBOV'].div(ibov['IBOV'].iloc[0]).mul(100)
+ibov
+
+plt.plot(norm['Portfolio'])
+plt.plot(ibov['Ibov'])
+plt.legend(['Portfolio - Size', 'Ibov'])
+plt.show()
