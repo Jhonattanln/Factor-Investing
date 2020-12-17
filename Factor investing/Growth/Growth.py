6a046e90432684d40efa4ac3c83b8e674e8107fe
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-12-17 19:14:53 -0300

    Update Growth.py

--- Factor investing/Growth/Growth.py
+++ Factor investing/Growth/Growth.py
@@ -3,4 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pandas_datareader import data
 
-lpa = pd.read_excel(r'LPA.xlsx')
+lpa = pd.read_excel(r'C:\Users\Jhona\OneDrive - Grupo Marista\Projetos\Factor Investing\Factor-Investing\Factor investing\Growth\LPA.xlsx',
+                    parse_dates=True, index_col=0)
+
+###################################################### LPA ########################################################
+
+def columns(df):
+    df.columns = df.columns.str[-6:]
+    return df
+columns(lpa)
+
+lpa.replace('-', np.nan, inplace=True)
+
+lpa = lpa.pct_change()
+
+data_lpa = {}
+
+for i in lpa:
+    data_lpa[i] = lpa[i].mean()
+
+data_lpa = pd.DataFrame.from_dict(data, orient='index')
+data_lpa.sort_values(by=[0], ascending=False, inplace=True)
+data_lpa
