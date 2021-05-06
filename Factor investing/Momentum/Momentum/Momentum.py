b7e09b0445bb58ee5b2aa0df8fda6fb5d0d8d2f4
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2021-05-05 23:25:39 -0300

    Update Momentum.py

--- Factor investing/Momentum/Momentum/Momentum.py
+++ Factor investing/Momentum/Momentum/Momentum.py
@@ -94,7 +94,7 @@ stocks = [
 
 df = pd.DataFrame()
 for i in stocks:
-    df[i] = data.DataReader(i, data_source='yahoo', start='2020-01-01')['Adj Close']
+    df[i] = data.DataReader(i, data_source='yahoo', start='2020-01-01', end = '2020-12-31')['Adj Close']
 
 weights = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 returns = df.pct_change().dropna()
@@ -112,7 +112,7 @@ plt.legend(loc='lower left')
 plt.show()
 
 ### Portfolio vs IBOV
-ibov = data.DataReader('^BVSP', data_source='yahoo', start='2020-01-01')
+ibov = data.DataReader('^BVSP', data_source='yahoo', start='2020-01-01', end = '2020-12-31')
 ibov.rename(columns = {'Adj Close':'IBOV'}, inplace=True)
 ibov.drop(ibov.columns[[0,1,2,3,4]], axis=1, inplace=True)
 ibov['Ibov'] = ibov['IBOV'].div(ibov['IBOV'].iloc[0]).mul(100)
@@ -122,3 +122,6 @@ plt.plot(norm['Portfolio'])
 plt.plot(ibov['Ibov'])
 plt.legend(['Portfolio - Momentum', 'Ibov'])
 plt.show()
+
+final = pd.concat([norm['Portfolio'], ibov['Ibov']], axis=1)
+final.to_excel('Mome.xlsx')
